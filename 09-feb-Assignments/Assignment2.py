5f6af7a500326499fe8fb6b821b9a678b2d09ac4
Harshitha <89914609+Harshitha129@users.noreply.github.com>
2022-02-13 12:02:09 +0530

    Add files via upload

---
+++ 09-feb-Assignments/Assignment2.py
@@ -0,0 +1,6 @@
+# program to find given number is even or odd
+num=int(input('Enter a number: '))
+if (num % 2) == 0:
+    print('%d is an even number',num)
+else:
+    print('%d is an odd number',num)
---
+++ 09-feb-Assignments/Assignments1.py
@@ -0,0 +1,7 @@
+# program to find biggest of two numbers
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+if(x >= y):
+  print('the greatest number is: ',x)
+else:
+  print('the greatest number is: ',y)
