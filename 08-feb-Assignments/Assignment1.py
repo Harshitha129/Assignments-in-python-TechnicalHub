639157c30938ffa62f968c6c43a5d57712d6f340
Harshitha <89914609+Harshitha129@users.noreply.github.com>
2022-02-13 12:01:47 +0530

    Add files via upload

---
+++ 08-feb-Assignments/Assignment1.py
@@ -0,0 +1,15 @@
+#program on comparison operators
+a=10
+b=20
+c=a<b
+print('a is lesser than b:- ',c)
+d=a>b
+print('a is greater than b:- ',d)
+e=a>=b
+print('a is greater than or equal to b:- ',e)
+f=a<=b
+print('a is less than or equal to b:- ',f)
+g=a==b
+print('a is equal to b:- ',g)
+h=a!=b 
+print('a is not equal to b:- ',h)
---
+++ 08-feb-Assignments/Assignment2.py
@@ -0,0 +1,5 @@
+#converting kilometres into centimetres
+a=50
+# 1km = 1,00,000cm
+x=a*100000
+print('5o kilometres in centimetres is: ',x)
