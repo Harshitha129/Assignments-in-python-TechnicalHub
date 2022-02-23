d5043f9e75ddc4c6812e761267e415da0242449f
Harshitha <89914609+Harshitha129@users.noreply.github.com>
2022-02-23 21:09:30 +0530

    Add files via upload

---
+++ 23-feb-Assignments/max and min from list.py
@@ -0,0 +1,6 @@
+a_list = [62, 78, 93, 56, 43,76]
+print(a_list)
+maximum = max(a_list)
+print("max: ",maximum)
+minimum = min(a_list)
+print("min: ",minimum)
---
+++ 23-feb-Assignments/reverse of list.py
@@ -0,0 +1,7 @@
+n = [1, 2, 3, 4, 5];     
+print("Array: ");    
+for i in range(0, len(n)):    
+    print(n[i]),     
+print("Array in reverse order: ");    
+for i in range(len(n)-1, -1, -1):     
+    print(n[i])   
